414c0593b9be30aec87e67d082efc12a3676f273
sampark1234 <sampark1234@wavus.co.kr>
2023-12-05 16:04:02 +0900

    apply message source code on basic config

--- src/main/java/me/dailycode/spring/legacy/controller/SomeController.java
+++ src/main/java/me/dailycode/spring/legacy/controller/SomeController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.dailycode.spring.legacy.dto.SampleDTO;
 import me.dailycode.spring.legacy.service.SampleService;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +17,19 @@ import java.util.Map;
 public class SomeController {
 
     private final SampleService service;
+    private final MessageSource messageSource;
 
     @GetMapping("/hello-world-json")
     public Map<String, Object> getDefaultInfo() {
         log.info("giving default Value");
         String currentTimeStamp = service.getCurrentTimeString("param");
         SampleDTO sampleData  = service.getSampleData();
+        String message = messageSource.getMessage("search.keyword", new Object[]{}, LocaleContextHolder.getLocale());
         return Map.of(
                 "hello", "world",
                 "currentTimeStamp", currentTimeStamp,
-                "sampleData", sampleData
+                "sampleData", sampleData,
+                "messageSourceMsg", message
         );
     }
 }
